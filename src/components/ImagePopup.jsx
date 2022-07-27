905748452f254a4a2d693d921802eedd7d1f11cd
touchresponce <touchresponce71@gmail.com>
2022-07-27 21:59:43 +0300

    fix: условие класса

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -1,6 +1,6 @@
 export default function ImagePopup({ card, onClose }) {
   return (
-    <div className={`popup popup-image ${card.link ? 'popup_opened' : ''}`}>
+    <div className={`popup popup-image ${card.link && 'popup_opened'}`}>
       <figure className="figure">
         <img className="popup__image" src={card.link} alt={card.name}></img>
         <figcaption className="popup__figcaption">{card.name}</figcaption>
